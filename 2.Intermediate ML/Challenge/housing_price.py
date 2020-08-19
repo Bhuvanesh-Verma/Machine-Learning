7c1c3d29f11ea0266a0febf289150793b8050158
Bhuvanesh-Verma <workoholic24x7@gmail.com>
2020-08-19 13:25:01 +0530

    Housing Price Challenge
    Housing Price Competition
    Missing Numerical Data is imputed with Imputation Extension method
    Categorical Data is preprocessed using Ordinal Labeling and One-Hot encoding

--- 2.Intermediate ML/Challenge/housing_price.py
+++ 2.Intermediate ML/Challenge/housing_price.py
@@ -7,10 +7,8 @@ Created on Tue Aug 18 15:07:54 2020
 
 
 import pandas as pd 
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.impute import SimpleImputer
 
 def calcPredictions(trainX,trainY,x):
@@ -36,9 +34,12 @@ def calcPredictions(trainX,trainY,x):
     return predictions
 
 
+        
+   
 data = pd.read_csv("../Datasets/housing_price/train.csv",index_col='Id')
 
 test = pd.read_csv("../Datasets/housing_price/test.csv",index_col='Id')
+
 T = test 
 Y = data.SalePrice
 data.drop(['SalePrice'],axis=1,inplace=True)
@@ -49,56 +50,62 @@ X =data
 T.drop(['Electrical'],axis=1,inplace=True)
 X.drop(['Electrical'],axis=1,inplace=True)
 
-# find column with missing data
-
-cols_with_train_missing_data = [col for col in X.columns if X[col].isnull().any()]
-
-cols_with_test_missing_data = [col for col in T.columns if T[col].isnull().any()]
+# find column with missing data in dataset
 
 
-uncommon_na_cols = set(cols_with_test_missing_data)-set(cols_with_train_missing_data)
-common_na_cols = set(cols_with_train_missing_data)-set(cols_with_test_missing_data)
+cols_with_num_missing_data_T = [col for col in T.columns if T[col].dtype in 
+                ['int64','float64'] and T[col].isnull().any()]
+cols_with_num_missing_data_X = [col for col in X.columns if X[col].dtype in 
+                ['int64','float64'] and X[col].isnull().any()]
 
-# I have choosen to keep only those na_columns which are common in both training and testing 
-# dataset. Testing contain all na_columns which are in training. So I can impute all those
-# columns but for this model I will pick common na_columns
 
+# remove missing data with string entries
+cols_with_object_missing_data_T= [col for col in T.columns if T[col].dtype =='object'
+                                 and T[col].isnull().any()]
+
+cols_with_object_missing_data_X = [col for col in X.columns if X[col].dtype =='object'
+                                 and X[col].isnull().any()]
+cols_to_remove = cols_with_object_missing_data_T + cols_with_object_missing_data_X
 
 X_copy =X.copy()
-X_copy.drop(uncommon_na_cols,axis=1)
-T_copy = T.copy()
-T_copy.drop(uncommon_na_cols,axis=1)
+X_copy = X_copy.drop(cols_to_remove,axis=1)
 
+T_copy = T.copy()
+T_copy = T_copy.drop(cols_to_remove,axis=1)
 
 
 #Lets use Imputation Extension to deal with missing data
 cols_for_na = []
-numeric_cols = []
+numeric_na_cols = cols_with_num_missing_data_T + cols_with_num_missing_data_X
 
-for col in cols_with_train_missing_data:
-    if X_copy[col].dtype in ['int64','float64']:
-        numeric_cols.append(col)
-        name = col + "_was_missing"
-        X_copy[name] = X_copy[col].isnull()
-        T_copy[name] = T_copy[col].isnull()
-        cols_for_na.append(name)
+
+
+for col in numeric_na_cols:
+    name = col + "_was_missing"
+    X_copy[name] = X_copy[col].isnull()
+    T_copy[name] = T_copy[col].isnull()
+    cols_for_na.append(name)
+    
 
 imputer = SimpleImputer()
-imputed_train_data = pd.DataFrame(imputer.fit_transform(X_copy[numeric_cols]))
-imputed_test_data = pd.DataFrame(imputer.transform(T_copy[numeric_cols]))
+imputed_train_data = pd.DataFrame(imputer.fit_transform(X_copy[numeric_na_cols]))
+imputed_test_data = pd.DataFrame(imputer.transform(T_copy[numeric_na_cols]))
 
 
-imputed_train_data.columns = X_copy[numeric_cols].columns
-imputed_test_data.columns = T_copy[numeric_cols].columns
+imputed_train_data.index = X_copy.index
+imputed_test_data.index = T_copy.index
 
-X_copy.drop(numeric_cols,axis=1)
-T_copy.drop(numeric_cols,axis=1)
+imputed_train_data.columns = X_copy[numeric_na_cols].columns
+imputed_test_data.columns = T_copy[numeric_na_cols].columns
 
+X_copy = X_copy.drop(numeric_na_cols,axis=1)
+T_copy = T_copy.drop(numeric_na_cols,axis=1)
 
 
 X_copy = pd.concat([X_copy,imputed_train_data],axis=1)
-T_copy = pd.concat([X_copy,imputed_test_data],axis=1)
-print(X_copy)
+T_copy = pd.concat([T_copy,imputed_test_data],axis=1)
+
+
 
 # Lets deal with categorical variables
 
@@ -106,8 +113,36 @@ print(X_copy)
 
 temp = (X_copy.dtypes == 'object')
 temp1 = (T_copy.dtypes == 'object')
-print(set(temp) == set(temp1))
 object_cols = temp[temp].index
 
+# We will use one-hot encoding to only those columns with less than 10 cardinality
+unique_objects = list(map(lambda col: X_copy[col].nunique(),object_cols))
+temp = dict(zip(object_cols,unique_objects))
+
+low_card_cols = [col for col in object_cols if X_copy[col].nunique() <10]
+high_card_cols = list(set(object_cols) - set(low_card_cols))
 
 
+
+# Encoding with One-Host Encoder
+encoder = OneHotEncoder(sparse=False,handle_unknown= 'ignore')
+temp_train = X_copy.drop(high_card_cols,axis=1)
+temp_valid = T_copy.drop(high_card_cols,axis=1)
+
+one_hot_train_cols = pd.DataFrame(encoder.fit_transform(X_copy[low_card_cols]))
+one_hot_test_cols = pd.DataFrame(encoder.transform(T_copy[low_card_cols]))
+
+one_hot_train_cols.index = X_copy.index
+one_hot_test_cols.index = T_copy.index
+
+temp_train = temp_train.drop(low_card_cols,axis=1)
+temp_valid = temp_valid.drop(low_card_cols,axis=1)
+
+OH_X_train = pd.concat([temp_train,one_hot_train_cols],axis =1)
+OH_X_valid = pd.concat([temp_valid,one_hot_test_cols],axis=1)
+
+pred = calcPredictions(OH_X_train,Y,OH_X_valid)
+
+
+output = pd.DataFrame({'Id': test.index,'SalePrice': pred})
+output.to_csv('submission.csv', index=False)
---
+++ 2.Intermediate ML/Challenge/housing_price_2.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 19 10:32:05 2020
+
+@author: Le Champion
+"""
+
+
+import pandas as pd 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+
+def calcPredictions(trainX,trainY,x):
+    '''
+    This function use RandomForestRegressor to build a model using
+    training data train_X and train_Y. max_leaf_nodes parameter can be adjusted 
+    to get better results. I have tried with 10,100,400,500,700 and 400
+    gave best results. After fitting model we predict results using x dataset.
+    Parameters
+    ----------
+    train_X : dataset for training.
+    train_Y : target SalePrice for training .
+    x : dataset for testing.
+
+    Returns
+    -------
+    predictions : predictions array.
+
+    '''
+    model = RandomForestRegressor(max_leaf_nodes=400,random_state=1)
+    model.fit(trainX,trainY)
+    predictions = model.predict(x)
+    return predictions
+
+def encodingOrdinalLabel(df):
+    condition_map = {'Ex':5,'Gd':4,'TA':3,'Fa':2,'Po':1}
+    slope_map = {'Gtl':1,'Mod':2,'Sev':3}
+    paved_map = {'Y':3,'P':2,'N':1}
+    lost_shape_map = {'Reg':4,"IR1":3,"IR2":2,"IR3":1}
+    
+    object_df = pd.DataFrame(df)
+    object_df['temp_exter_cond'] = object_df.ExterCond.map(condition_map)
+    object_df['temp_land_slope'] = object_df.LandSlope.map(slope_map)
+    object_df['temp_paved_drive'] = object_df.PavedDrive.map(paved_map)
+    object_df['temp_lot_shape'] = object_df.LotShape.map(lost_shape_map)
+    object_df['temp_exter_qual'] = object_df.ExterQual.map(condition_map)
+    object_df['temp_heating_qc'] = object_df.HeatingQC.map(condition_map)
+    
+    temp = ['temp_exter_cond','temp_land_slope','temp_paved_drive','temp_lot_shape',
+            'temp_exter_qual','temp_heating_qc']
+    object_df = object_df.drop(cols_for_label,axis=1)
+    object_df['ExterCond'] = object_df['temp_exter_cond'] 
+    object_df['LandSlope'] = object_df['temp_land_slope']
+    object_df['PavedDrive'] = object_df['temp_paved_drive']
+    object_df['LotShape'] = object_df['temp_lot_shape']
+    object_df['ExterQual'] = object_df['temp_exter_qual']
+    object_df['HeatingQC'] = object_df['temp_heating_qc']
+    
+    object_df = object_df.drop(temp,axis=1)
+    return object_df
+    
+    
+    
+data = pd.read_csv("../Datasets/housing_price/train.csv",index_col='Id')
+
+test = pd.read_csv("../Datasets/housing_price/test.csv",index_col='Id')
+
+T = test 
+Y = data.SalePrice
+data.drop(['SalePrice'],axis=1,inplace=True)
+X =data
+features = ["PoolArea","TotRmsAbvGrd","2ndFlrSF","1stFlrSF",
+            "YearBuilt","YearRemodAdd","LotArea","MSSubClass","OpenPorchSF","Fireplaces",
+            "GrLivArea","YrSold","3SsnPorch","WoodDeckSF","HalfBath","FullBath",
+        "EnclosedPorch","OverallQual","OverallCond", ]
+
+
+#  *******************************NUMERIC**DATA*************************************
+
+#Dealing with numeric data
+num_cols_to_impute = ['LotFrontage','BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF',
+                   'TotalBsmtSF', 'BsmtFullBath','BsmtHalfBath',
+                    'GarageCars', 'GarageArea']
+
+total_features = features + num_cols_to_impute
+X_copy = X[total_features]
+T_copy = T[total_features]
+
+for col in num_cols_to_impute:
+    name = col + "_was_missing"
+    X_copy[name] = X_copy[col].isnull()
+    T_copy[name] = T_copy[col].isnull()
+
+imputer = SimpleImputer()
+imputed_num_train_data = pd.DataFrame(imputer.fit_transform(
+    X_copy[num_cols_to_impute]))
+imputed_num_test_data = pd.DataFrame(imputer.fit_transform(
+    T_copy[num_cols_to_impute]))
+imputed_num_test_data.index = T.index
+imputed_num_train_data.index = X.index
+
+imputed_num_train_data.columns = X_copy[num_cols_to_impute].columns
+imputed_num_test_data.columns = T_copy[num_cols_to_impute].columns
+
+X_copy = X_copy.drop(num_cols_to_impute,axis = 1)
+T_copy = T_copy.drop(num_cols_to_impute,axis = 1)
+
+new_X = pd.concat([X_copy,imputed_num_train_data],axis=1)
+new_T = pd.concat([T_copy,imputed_num_test_data],axis=1)
+
+#  ***************************CATEGORICAL**DATA*****************************
+
+#Dealing with Categorical Data
+
+cols = [col for col in T.columns if T[col].dtype == 'object'
+        and not( T[col].isnull().any())]
+
+# training object variables with no NA 
+train_not_na_cols = ['MSZoning', 'Street', 'LotShape', 'LandContour', 
+                      'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 
+                      'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 
+                      'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 
+                      'ExterQual', 'ExterCond', 'Foundation', 'Heating', 
+                      'HeatingQC', 'CentralAir', 'KitchenQual', 'Functional', 
+                      'PavedDrive', 'SaleType', 'SaleCondition']
+ 
+#test object variable with no NA
+test_not_na_cols = ['Street', 'LotShape', 'LandContour', 'LotConfig', 
+                     'LandSlope', 'Neighborhood', 'Condition1', 'Condition2',
+                     'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 
+                     'ExterQual', 'ExterCond', 'Foundation', 'Heating', 
+                     'HeatingQC', 'CentralAir', 'Electrical', 'PavedDrive', 
+                     'SaleCondition']
+
+# common objects in both dataset with no na
+non_na = list(set(train_not_na_cols) and set(test_not_na_cols))
+
+
+non_na.remove('Electrical') # includes NA data
+non_na.remove('Neighborhood') # too many variable for Encoding
+
+cols_for_label = ['ExterCond','LandSlope','PavedDrive','LotShape','ExterQual',
+                  'HeatingQC']
+cols_for_OH = list(set(non_na) - set(cols_for_label))
+
+# creating maps for label encoding
+
+train_df = encodingOrdinalLabel(X[cols_for_label])
+test_df = encodingOrdinalLabel(T[cols_for_label])
+
+new_X = pd.concat([new_X,train_df],axis=1)
+new_T = pd.concat([new_T,test_df],axis=1)
+
+# We will use one-hot encoding to only those columns with less than 10 cardinality
+unique_objects = list(map(lambda col: X[col].nunique(),cols_for_OH))
+temp = dict(zip(cols_for_OH,unique_objects))
+print(sorted(temp.items(),key=lambda x : x[1])) #it shows all cols in cols_for_OH
+# have cardinality less than 10
+
+
+# Encoding with One-Host Encoder
+encoder = OneHotEncoder(sparse=False,handle_unknown= 'ignore')
+one_hot_train_cols = pd.DataFrame(encoder.fit_transform(X[cols_for_OH]))
+one_hot_test_cols = pd.DataFrame(encoder.transform(T[cols_for_OH]))
+
+one_hot_train_cols.index = X.index
+one_hot_test_cols.index = T.index
+
+
+new_X = pd.concat([new_X,one_hot_train_cols],axis=1)
+new_T = pd.concat([new_T,one_hot_test_cols],axis=1)
+
+pred = calcPredictions(new_X,Y,new_T)
+
+output = pd.DataFrame({'Id': test.index,'SalePrice': pred})
+output.to_csv('submission.csv', index=False)
